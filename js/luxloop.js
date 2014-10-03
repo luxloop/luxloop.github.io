6ad8334d3453f5b7095f0cf1ce9819ddb68ac250
Ivaylo Getov <ivaylo.getov@gmail.com>
2014-10-03 00:58:50 -0400

    test

--- js/luxloop.js
+++ js/luxloop.js
@@ -3,5 +3,18 @@ $(document).ready(function() {
 	      "/img/001.jpg"
 	    , "/img/002.jpg"
 	    , "/img/003.png"
-	  ], {duration: 3000, fade: 750});
+	    , "/img/004.png"
+	    , "/img/005.jpg"
+	    , "/img/006.jpg"
+	    , "/img/007.jpg"
+	    , "/img/008.jpg"
+	    , "/img/009.jpg"
+	    , "/img/010.jpg"
+	    , "/img/011.jpg"
+	    , "/img/012.jpg"
+	    , "/img/013.png"
+	    , "/img/014.png"
+	    , "/img/015.jpg"
+	    , "/img/016.jpg"
+	  ], {duration: 5500, fade: 1000});
 });
