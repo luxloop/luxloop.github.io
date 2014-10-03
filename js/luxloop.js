563e8676356ac35244b3f0c65c3cc68dee990acf
Ivaylo Getov <ivaylo.getov@gmail.com>
2014-10-03 01:12:55 -0400

    title headline

--- js/luxloop.js
+++ js/luxloop.js
@@ -1,21 +1,23 @@
 $(document).ready(function() {
-	 $.backstretch([
-	      "/img/001.jpg"
-	    , "/img/002.jpg"
-	    , "/img/003.png"
-	    , "/img/004.png"
-	    , "/img/005.jpg"
-	    , "/img/006.jpg"
-	    , "/img/007.jpg"
-	    , "/img/008.jpg"
-	    , "/img/009.jpg"
-	    , "/img/010.jpg"
-	    , "/img/011.jpg"
-	    , "/img/012.jpg"
-	    , "/img/013.png"
-	    , "/img/014.png"
-	    , "/img/015.jpg"
-	    , "/img/016.jpg"
-	    , "/img/018.jpg"
-	  ], {duration: 5500, fade: 1000});
+	$("#nameText").fitText();
+
+	$.backstretch([
+		  "/img/001.jpg"
+		, "/img/002.jpg"
+		, "/img/003.png"
+		, "/img/004.png"
+		, "/img/005.jpg"
+		, "/img/006.jpg"
+		, "/img/007.jpg"
+		, "/img/008.jpg"
+		, "/img/009.jpg"
+		, "/img/010.jpg"
+		, "/img/011.jpg"
+		, "/img/012.jpg"
+		, "/img/013.png"
+		, "/img/014.png"
+		, "/img/015.jpg"
+		, "/img/016.jpg"
+		, "/img/018.jpg"
+	], {duration: 5500, fade: 1000});
 });
