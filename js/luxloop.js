df0607293c2922625e1ca6f24b3a144cbe953ecf
Ivaylo Getov <ivaylo.getov@gmail.com>
2014-10-21 15:17:43 -0400

    ..

--- js/luxloop.js
+++ js/luxloop.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
             // }, 200);
 
             if(index == 1 && direction =='down'){
-                $("body").scrollTop(0);
+                $("body").scrollTop(1);
                 $("#menuBarBot").fadeOut();
                 $("#menuBarTop").fadeIn(1200);
             }
