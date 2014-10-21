8ffa3d273ebc6b7a09fcbe562555645349ad9bb2
Ivaylo Getov <ivaylo.getov@gmail.com>
2014-10-21 17:29:27 -0400

    took out mobile slideshow

--- js/luxloop.js
+++ js/luxloop.js
@@ -46,22 +46,24 @@ $(document).ready(function() {
         }
     });
 
-    $("#slideshowBg").backstretch([
-          "img/017.jpg"
-        , "img/001.jpg"
-        , "img/002.jpg"
-        , "img/003.png"
-        , "img/004.png"
-        , "img/005.jpg"
-        , "img/006.jpg"
-        , "img/007.jpg"
-        , "img/008.jpg"
-        , "img/010.jpg"
-        , "img/011.jpg"
-        , "img/012.jpg"
-        , "img/013.png"
-        , "img/014.png"
-    ], {duration: 5500, fade: 1000});
+    if (!isMobile) {
+        $("#slideshowBg").backstretch([
+              "img/017.jpg"
+            , "img/001.jpg"
+            , "img/002.jpg"
+            , "img/003.png"
+            , "img/004.png"
+            , "img/005.jpg"
+            , "img/006.jpg"
+            , "img/007.jpg"
+            , "img/008.jpg"
+            , "img/010.jpg"
+            , "img/011.jpg"
+            , "img/012.jpg"
+            , "img/013.png"
+            , "img/014.png"
+        ], {duration: 5500, fade: 1000});
+    };
 
     var moving = false;
     var blogSlide = 1;
