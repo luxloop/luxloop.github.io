fe7e6be5bda365925c816c9481bf74f70aa6a674
Ivaylo Getov <ivaylo.getov@gmail.com>
2016-03-13 23:52:49 -0700

    fix safari issues

--- tests/gs/js/main.js
+++ tests/gs/js/main.js
@@ -1,19 +1,28 @@
+var leftDown = false;
+var rightDown = false;
+var mostRecent = ""
+
 $(document).ready(function() {
-//console.log("ready");
+  //console.log("ready");
+  animate()
 });
 
 $(document).keydown(function(e) {
+    // console.log(e.keyCode);
     switch (e.keyCode) {
         case 37:
             e.preventDefault();
-            $("#vidWindow video").addClass("shift");
+            //$("#vidWindow video").addClass("shift");
+            leftDown = true;
+            mostRecent = "l";
             break;
         case 39:
-            e.preventDefault();
-            $("#vidWindow video").addClass("shift2");
+            //e.preventDefault();
+            //$("#vidWindow video").addClass("shift2");
+            rightDown = true;
+            mostRecent = "r";
             break;
         default:
-            console.log(e.keyCode);
             break;
     }
 });
@@ -27,17 +36,20 @@ $("#imgCover").click(function(e) {
 });
 
 $(document).keyup(function(e) {
+    mostRecent = "";
+    // console.log(e.keyCode);
     switch (e.keyCode) {
         case 37:
             e.preventDefault();
-            $("#vidWindow video").removeClass("shift");
+            //$("#vidWindow video").removeClass("shift");
+            leftDown = false;
             break;
         case 39:
             e.preventDefault();
-            $("#vidWindow video").removeClass("shift2");
+            //$("#vidWindow video").removeClass("shift2");
+            rightDown = false;
             break;
         default:
-            console.log(e.keyCode);
             break;
     }
 });
@@ -58,6 +70,23 @@ theVid.onended = function() {
     $("#imgCover").removeClass("hidden");
     //$("#imgCover").fadeIn(2000);
 };
+
+function animate() {
+  if (mostRecent !== "") {
+    if (mostRecent == "l") {
+      $("#vidWindow").scrollTop(406);
+    } else if (mostRecent == "r") {
+      $("#vidWindow").scrollTop(808);
+    }
+  } else if (leftDown) {
+    $("#vidWindow").scrollTop(406);
+  } else if (rightDown) {
+    $("#vidWindow").scrollTop(808);
+  } else {
+    $("#vidWindow").scrollTop(1);
+  }
+  requestAnimationFrame(animate);
+}
 
 
 // Avoid `console` errors in browsers that lack a console.
@@ -83,4 +112,32 @@ theVid.onended = function() {
     }
 }());
 
-// Place any jQuery/helper plugins in here.
+// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+
+// MIT license
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] ||
+                                      window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
