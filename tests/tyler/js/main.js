e77a721691727faac73e4d84b175520fdbe717d0
Ivaylo Getov <ivaylo.getov@gmail.com>
2016-06-16 19:04:21 -0700

    update tyler shields video

--- tests/tyler/js/main.js
+++ tests/tyler/js/main.js
@@ -1,28 +1,58 @@
+// Gypsy Sport - Al La Do - Interactive Teaser
+// (c) 2016 Luxloop
+var hasLoaded = false;
+var shift = false;
+var offset = 200;
+var timeOut;
+var playing = false;
+
 $(document).ready(function() {
+  //console.log("ready");
+  // timeout = setTimeout(setReadyToPlay, 10000);
+  $("#theVideo").on('canplaythrough', canPlayThrough);
+  $("#theVideo").attr('src','vid/tylertest.mp4');
+  resizeContainer()
+  animate()
 
 });
 
 $(document).keydown(function(e) {
-    if (e.keyCode == 72) {
+    console.log(e.keyCode);
+    switch (e.keyCode) {
+      case 72:
         e.preventDefault();
-        //console.log("on");
-        $("#vidWindow video").addClass("shift");
-    }   
+        if (playing) {
+          shift = true;
+        }
+        break;
+      default:
+        break;
+    }
 });
 
 $("#imgCover").click(function(e) {
     e.preventDefault();
-    $(this).addClass("hidden");
-    var vid = document.getElementById("theVideo");
-    vid.play();
+    if (hasLoaded) {
+      $(this).addClass("hidden");
+      //$(this).fadeOut(2000);
+      var vid = document.getElementById("theVideo");
+      vid.play();
+      playing = true;
+    }
 });
 
 $(document).keyup(function(e) {
-    if (e.keyCode == 72) {
+    mostRecent = "";
+    // console.log(e.keyCode);
+    switch (e.keyCode) {
+      case 72:
         e.preventDefault();
-        //console.log("off");
-        $("#vidWindow video").removeClass("shift");
-    }   
+        //$("#vidWindow video").removeClass("shift");
+        shift = false;
+        break;
+      default:
+        break;
+    }
 });
 
 $("#vidWindow video").click(function(e) {
@@ -30,8 +60,10 @@ $("#vidWindow video").click(function(e) {
     var vid = document.getElementById("theVideo");
     if(vid.paused){
         vid.play();
+        playing = true;
     } else {
         vid.pause();
+        playing = false;
     }
 });
 
@@ -39,7 +71,64 @@ var theVid = document.getElementById("theVideo");
 theVid.onended = function() {
     //alert("The video has ended");
     $("#imgCover").removeClass("hidden");
+    //$("#imgCover").fadeIn(2000);
 };
+
+function animate() {
+
+  if (shift) {
+    $("#vidWindow").scrollTop(offset);
+    // console.log("OFF")
+  } else {
+    $("#vidWindow").scrollTop(1);
+    // console.log("ON")
+  }
+  requestAnimationFrame(animate);
+}
+
+//Throttled on-resize handler
+on_resize(function() {
+    resizeContainer()
+});
+
+function canPlayThrough() {
+  console.log("video can play")
+  clearTimeout(timeOut)
+  setReadyToPlay()
+}
+
+function setReadyToPlay() {
+  $("#imgCover").removeClass('loading')
+  $("#imgCover").addClass('loaded')
+  hasLoaded = true;
+}
+
+function resizeContainer() {
+  //*
+  var scale = 0.5625;
+  var scale2up = 1.125;
+  var winWidth = window.innerWidth;
+  var width;
+
+  if (winWidth > 1700) {
+    width = winWidth * 0.6;
+  } else if (winWidth > 1400) {
+    width = winWidth * 0.7;
+  } else if (winWidth > 600) {
+    width = winWidth * 0.8;
+  } else {
+    width = winWidth * 0.9;
+  }
+  console.log(winWidth)
+  console.log(width)
+
+  if (width !== undefined) {
+    $("body #vidWindow").css({"width":width,"height":width*scale - 5})
+    $("body #theVideo").css({"width":width,"height":width*scale2up})
+    offset = width * scale;
+  }
+  //*/
+}
 
 
 // Avoid `console` errors in browsers that lack a console.
@@ -65,4 +154,35 @@ theVid.onended = function() {
     }
 }());
 
-// Place any jQuery/helper plugins in here.
+// debulked onresize handler
+function on_resize(c,t){onresize=function(){clearTimeout(t);t=setTimeout(c,250)};return c};
+
+// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+
+// MIT license
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] ||
+                                      window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
