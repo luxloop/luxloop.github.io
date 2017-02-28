33dcbcc93e2a52fd7e9733302636fb21c50cbdbd
Ivaylo Getov <contact@ivaylogetov.com>
2017-02-27 17:47:49 -0800

    add social images and init main js file

---
+++ source/_js/luxloop.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $(".hiddenEmail").html(rot13rot5Encode('<n uers="znvygb:uryyb@yhkybbc.pbz">uryyb@yhkybbc.pbz</n>'));
+  $(".hiddenPhoneLA").html(rot13rot5Encode('<n uers="gry:+68657111092" pynff="yvaxNavz">865.711.1092 (YN)</n>'));
+  $(".hiddenPhoneNY").html(rot13rot5Encode('<n uers="gry:+61919393851" pynff="yvaxNavz">191.939.3851 (AL)</n>'));
+});
+
