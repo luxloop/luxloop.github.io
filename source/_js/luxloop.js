efccebd3208c2890dcab72af0a463760c30f66b5
Ivaylo Getov <contact@ivaylogetov.com>
2017-08-03 11:47:31 -0700

    add SVG logo to project pages

--- source/_js/luxloop.js
+++ source/_js/luxloop.js
@@ -10,6 +10,7 @@
   $(document).ready(function() {
     $(window).scrollIntent();
     $('body').addClass('showBody')
+    processSVG();
     initParallax();
     initCover();
     sizeCover();
@@ -268,4 +269,36 @@
     var dest = "https://lux-loop.us9.list-manage.com/subscribe?u=932da86c44803a133780827a6&id=85c35a3d5a&MERGE0="+encodeURIComponent(input)
     window.open(dest)
   }
+
+  function processSVG() {
+    $('img.svg').each(function(){
+      var $img = jQuery(this);
+      var imgID = $img.attr('id');
+      var imgClass = $img.attr('class');
+      var imgURL = $img.attr('src');
+
+      $.get(imgURL, function(data) {
+          // Get the SVG tag, ignore the rest
+          var $svg = jQuery(data).find('svg');
+
+          // Add replaced image's ID to the new SVG
+          if(typeof imgID !== 'undefined') {
+              $svg = $svg.attr('id', imgID);
+          }
+          // Add replaced image's classes to the new SVG
+          if(typeof imgClass !== 'undefined') {
+              $svg = $svg.attr('class', imgClass+' replaced-svg');
+          }
+
+          // Remove any invalid XML tags as per http://validator.w3.org
+          $svg = $svg.removeAttr('xmlns:a');
+
+          // Replace image with new SVG
+          $img.replaceWith($svg);
+
+      }, 'xml');
+    });
+  }
+
+
 })();
